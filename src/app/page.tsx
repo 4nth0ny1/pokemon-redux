aa5e06c196bb5d66aa7e66099cf853f486dea144
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-28 11:20:53 -0500

    created SSRPokemonTable before redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
   const data = await req.json();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Pokemon Redux</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="pb-8">Pokemon Redux</h1>
       <PokemonTable pokemons={data} />
     </main>
   );
