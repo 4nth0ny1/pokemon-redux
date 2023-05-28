55a0a82de3e46b3479e2d9fef11f59dd9d5381da
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-28 11:17:34 -0500

    types.ts, created PokemonTable, rendered to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import PokemonTable from "@/components/PokemonTable";
+
 export default async function Home() {
   const req = await fetch("http://localhost:3000/api/search");
   const data = await req.json();
@@ -5,7 +7,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pokemon Redux</h1>
-      {JSON.stringify(data)}
+      <PokemonTable pokemons={data} />
     </main>
   );
 }
