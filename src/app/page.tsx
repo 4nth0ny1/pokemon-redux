03f5625121e45ebcf8a1a91881a9ae27303a0461
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-28 11:09:58 -0500

    fetched data from pokemon.json with route.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+export default async function Home() {
+  const req = await fetch("http://localhost:3000/api/search");
+  const data = await req.json();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pokemon Redux</h1>
+      {JSON.stringify(data)}
     </main>
   );
 }
