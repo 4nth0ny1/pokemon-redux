cf44221b6164d8d5757f3f4a7e81b9b58b501b0a
Anthony Catullo <anthonycatullo@oatslam.local>
2023-05-28 11:42:41 -0500

    created redux store, slice, index.ts, and are now using SSRPokemonTable with store data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import PokemonTable from "@/components/PokemonTable";
+import SSRPokemonTable from "@/components/SSRPokemonTable";
+
+import { store } from "@/store";
+import { setStartupPokemon } from "@/store/searchSlice";
 
 export default async function Home() {
   const req = await fetch("http://localhost:3000/api/search");
   const data = await req.json();
+  store.dispatch(setStartupPokemon(data));
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="pb-8">Pokemon Redux</h1>
-      <PokemonTable pokemons={data} />
+      <SSRPokemonTable />
     </main>
   );
 }
